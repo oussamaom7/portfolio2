48c8bfe3a73e27cea80df9bcd8e86f07662d7f99
oussamaom7 <oussama.maache@uit.ac.ma>
2025-03-21 17:35:19 +0100

    earth

--- src/components/sections/Contact.jsx
+++ src/components/sections/Contact.jsx
@@ -157,7 +157,7 @@ const Contact = () => {
   return (
     <Container>
       <Wrapper>
-        <EarthCanvas />
+       
         <Title>Contact</Title>
         <Desc>
           Feel free to reach out to me for any questions or opportunities!
